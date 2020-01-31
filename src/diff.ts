2696d8573dcbeb9e6d3376ec2a39e8493165ed6a
Sam <sam@developersam.com>
2020-01-30 22:19:23 -0500

    Factor out `shouldDiffBeIgnored`
    Prepare for detecting moved lines diff and produce a diff that has moved lines.

--- src/diff.ts
+++ src/diff.ts
@@ -5,17 +5,18 @@ const ignores: readonly string[] = ['yarn.lock', 'package-lock.json', 'Pods/', '
 const shouldBeIgnored = (path: string): boolean =>
   ignores.find(ignore => path.includes(ignore)) != null;
 
+const shouldDiffBeIgnored = ({ oldFileName, newFileName }: Diff.ParsedDiff): boolean =>
+  (oldFileName != null && shouldBeIgnored(oldFileName)) ||
+  (newFileName != null && shouldBeIgnored(newFileName));
+
 export default (diffString: string): number => {
   const parsedDiff = Diff.parsePatch(diffString);
   let significantChangedLines = 0;
-  parsedDiff.forEach(({ oldFileName, newFileName, hunks }) => {
-    if (
-      (oldFileName != null && shouldBeIgnored(oldFileName)) ||
-      (newFileName != null && shouldBeIgnored(newFileName))
-    ) {
-      console.log(`Change ${oldFileName} => ${newFileName} is not significant.`);
+  parsedDiff.forEach(diff => {
+    if (shouldDiffBeIgnored(diff)) {
       return;
     }
+    const { oldFileName, newFileName, hunks } = diff;
     const signficantLinesForOneFile = hunks
       .flatMap(hunk => hunk.lines)
       .filter(line => line[0] === '+' || line[0] === '-').length;
