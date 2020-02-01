6fd67868d53d6deb71473dd6cce07fdb525df105
Sam Zhou <sam@developersam.com>
2020-02-01 17:08:27 -0500

    Factor out `getDiffStatistics` in `diff.ts` (#7)
    ### Summary
    
    This diff factors out a `getDiffStatistics` function that accumulates an `addStatistics` and `deleteStatistics` multiset. They are useful for detecting moved lines in a future diff.
    
    ### Test
    
    All existing tests are passing, which shows that current correct behavior is preserved.

--- src/diff.ts
+++ src/diff.ts
@@ -9,6 +9,45 @@ const shouldDiffBeIgnored = ({ oldFileName, newFileName }: Diff.ParsedDiff): boo
   (oldFileName != null && shouldBeIgnored(oldFileName)) ||
   (newFileName != null && shouldBeIgnored(newFileName));
 
+type ChangeStatistics = {
+  readonly oldFileName: string;
+  readonly newFileName: string;
+  readonly addStatistics: Map<string, number>;
+  readonly deleteStatistics: Map<string, number>;
+};
+
+const getDiffStatistics = ({
+  oldFileName: oldFilenameOptional,
+  newFileName: newFilenameOptional,
+  hunks
+}: Diff.ParsedDiff): ChangeStatistics => {
+  const oldFileName = oldFilenameOptional ?? '';
+  const newFileName = newFilenameOptional ?? '';
+  const addStatistics = new Map<string, number>();
+  const deleteStatistics = new Map<string, number>();
+  hunks
+    .flatMap(hunk => hunk.lines)
+    .forEach(line => {
+      switch (line[0]) {
+        case '+': {
+          const whiteSpaceStrippedLine = line.substring(1).trim();
+          const existingCount = addStatistics.get(whiteSpaceStrippedLine) ?? 0;
+          addStatistics.set(whiteSpaceStrippedLine, existingCount + 1);
+          break;
+        }
+        case '-': {
+          const whiteSpaceStrippedLine = line.substring(1).trim();
+          const existingCount = deleteStatistics.get(whiteSpaceStrippedLine) ?? 0;
+          deleteStatistics.set(whiteSpaceStrippedLine, existingCount + 1);
+          break;
+        }
+        default:
+          break;
+      }
+    });
+  return { oldFileName, newFileName, addStatistics, deleteStatistics };
+};
+
 export default (diffString: string): number => {
   const parsedDiff = Diff.parsePatch(diffString);
   let significantChangedLines = 0;
@@ -16,10 +55,10 @@ export default (diffString: string): number => {
     if (shouldDiffBeIgnored(diff)) {
       return;
     }
-    const { oldFileName, newFileName, hunks } = diff;
-    const signficantLinesForOneFile = hunks
-      .flatMap(hunk => hunk.lines)
-      .filter(line => line[0] === '+' || line[0] === '-').length;
+    const { oldFileName, newFileName, addStatistics, deleteStatistics } = getDiffStatistics(diff);
+    const signficantLinesForOneFile =
+      Array.from(addStatistics.values()).reduce((accumulator, count) => accumulator + count, 0) +
+      Array.from(deleteStatistics.values()).reduce((accumulator, count) => accumulator + count, 0);
     console.log(
       `Change ${oldFileName} => ${newFileName} has ${signficantLinesForOneFile} lines diff.`
     );
