de506783f207f445e1115372287d6c0018455fac
Sam <sam@developersam.com>
2020-02-01 17:35:46 -0500

    Added tests on `mergeDiffStatistics`

--- src/diff.test.ts
+++ src/diff.test.ts
@@ -1,5 +1,95 @@
 import { spawnSync } from 'child_process';
-import computeSignificantLines from './diff';
+import computeSignificantLines, { mergeDiffStatistics } from './diff';
+
+it('mergeDiffStatistics with empty input works.', () => {
+  const result = mergeDiffStatistics([]);
+  const addStatistics = Array.from(result.addStatistics.entries());
+  const deleteStatistics = Array.from(result.deleteStatistics.entries());
+  expect(addStatistics).toEqual([]);
+  expect(deleteStatistics).toEqual([]);
+});
+
+it('mergeDiffStatistics with dummy list works.', () => {
+  const result = mergeDiffStatistics([
+    { oldFileName: '', newFileName: '', addStatistics: new Map(), deleteStatistics: new Map() },
+    { oldFileName: '', newFileName: '', addStatistics: new Map(), deleteStatistics: new Map() },
+    { oldFileName: '', newFileName: '', addStatistics: new Map(), deleteStatistics: new Map() },
+    { oldFileName: '', newFileName: '', addStatistics: new Map(), deleteStatistics: new Map() }
+  ]);
+  const addStatistics = Array.from(result.addStatistics.entries());
+  const deleteStatistics = Array.from(result.deleteStatistics.entries());
+  expect(addStatistics).toEqual([]);
+  expect(deleteStatistics).toEqual([]);
+});
+
+it('mergeDiffStatistics with singleton input works.', () => {
+  const result = mergeDiffStatistics([
+    {
+      oldFileName: '',
+      newFileName: '',
+      addStatistics: new Map([
+        ['a', 3],
+        ['b', 4]
+      ]),
+      deleteStatistics: new Map([
+        ['e', 4],
+        ['f', 5]
+      ])
+    }
+  ]);
+  const addStatistics = Array.from(result.addStatistics.entries());
+  const deleteStatistics = Array.from(result.deleteStatistics.entries());
+  expect(addStatistics).toEqual([
+    ['a', 3],
+    ['b', 4]
+  ]);
+  expect(deleteStatistics).toEqual([
+    ['e', 4],
+    ['f', 5]
+  ]);
+});
+
+it('mergeDiffStatistics with simple input works.', () => {
+  const result = mergeDiffStatistics([
+    {
+      oldFileName: '',
+      newFileName: '',
+      addStatistics: new Map([
+        ['a', 3],
+        ['b', 4]
+      ]),
+      deleteStatistics: new Map([
+        ['e', 4],
+        ['f', 5]
+      ])
+    },
+    {
+      oldFileName: '',
+      newFileName: '',
+      addStatistics: new Map([
+        ['a', 4],
+        ['b', 5],
+        ['c', 4]
+      ]),
+      deleteStatistics: new Map([
+        ['g', 7],
+        ['f', 5]
+      ])
+    }
+  ]);
+  const addStatistics = Array.from(result.addStatistics.entries());
+  const deleteStatistics = Array.from(result.deleteStatistics.entries());
+  expect(addStatistics).toEqual([
+    ['a', 7],
+    ['b', 9],
+    ['c', 4]
+  ]);
+  expect(deleteStatistics).toEqual([
+    ['e', 4],
+    ['f', 10],
+    ['g', 7]
+  ]);
+});
 
 it('Empty diff has 0 lines of change.', () => expect(computeSignificantLines('')).toBe(0));
 
