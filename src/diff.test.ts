01bf434062759abc8b7ce2bc3bcf85a58d095663
Sam Zhou <sam@developersam.com>
2020-02-01 17:15:31 -0500

    Add more integration tests on diff (#8)
    These tests are intended to show the old behavior of not special-casing moved lines. They are helpful to evaluate the impact of reducing count for moved lines in later diffs.

--- src/diff.test.ts
+++ src/diff.test.ts
@@ -11,6 +11,18 @@ const testOnRepo = (commit1: string, commit2: string, expected: number): void =>
 };
 
 testOnRepo(
+  '79fc97d7a9dd644914e3942170b8fd5a4d7f27fb',
+  'caacb1aeb2be692fe8803ac91ca7c04493830fcd',
+  15
+);
+
+testOnRepo(
+  'caacb1aeb2be692fe8803ac91ca7c04493830fcd',
+  'ebdffbe230e3a98cc2383fe09c8fbb853e6784fd',
+  22
+);
+
+testOnRepo(
   'ebdffbe230e3a98cc2383fe09c8fbb853e6784fd',
   '26af977281bb05191206e477ddb4fbf80d9e750b',
   8
